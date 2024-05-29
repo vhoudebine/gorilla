d1dc898e321495b4916bf6c4569ce682162aca93
Cedric Vidal <cedric@vidal.biz>
2024-05-29 08:32:17 +0000

    RAFT Display PDFs in notebooks

--- .devcontainer/raft/post-create.sh
+++ .devcontainer/raft/post-create.sh
@@ -14,3 +14,8 @@ pip install --upgrade pip
 
 echo "Installing requirements"
 pip install -r requirements.txt
+
+echo "Utilities to display PDFs in notebooks"
+sudo apt-update && \
+    sudo apt install -y libmagickwand-dev && \
+    sudo sed -i /PDF/d /etc/ImageMagick-6/policy.xml
